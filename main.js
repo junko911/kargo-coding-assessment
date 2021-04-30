6f96859b1f04902774e912b453a76e8569d5e787
junko911 <junkotahara911@gmail.com>
2021-04-29 22:05:40 -0400

    Extract to a function

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
-const phoneticNumbers = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"]
 const numbers = process.argv.slice(2)
 
-const parsedNumbers = numbers.map(num => {
-  const items = num.split("")
-  const parsedItems = items.map(item => phoneticNumbers[parseInt(item)])
-  return parsedItems.join("")
-})
+const convertNumbers = nums => {
+  const phoneticNumbers = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"]
 
-console.log(parsedNumbers.join(","))
+  const parsedNumbers = nums.map(num => {
+    const items = num.split("")
+    const parsedItems = items.map(item => phoneticNumbers[parseInt(item)])
+    return parsedItems.join("")
+  })
+  
+  return parsedNumbers.join(",")
+}
+
+console.log(convertNumbers(numbers))
